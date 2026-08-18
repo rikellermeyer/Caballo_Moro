7a27c40c82eca1f9136187bea034caab06488096
rikellermeyer <rileykellermeyer@gmail.com>
2026-08-18 15:32:15 -0500

    preplink good

--- code/popgen/preplink.py
+++ code/popgen/preplink.py
@@ -1,13 +1,14 @@
-#!/home/rk2643/miniforge3/envs/CMpop/bin/python
+#!/users/9/kell3262/miniforge3/envs/CMpop/bin/python
 
-## Note, between CM variant calling/hard_filtering and here, we are using different conda envs
+## Note, between CM variant calling/hard_filtering and here, we are using different conda env
 
 ## this does some prep work for vcf files for downstream PCA/GWAS analysis
-# First, rename chromosomes in original variant vcf file {genome}.biallelic.filtered.vcf.gz
-# Then, remove M2 from analysis
+# First, rename chromosomes in original variant vcf file 
+# Then, either remove Molino for GWAS or keep it in for admixture/PCA
 # Remove all chromosomes outside of 1-25
 # For pca, pair down to 2 nocalls and does LD filtering
 # Makes pca files and admixture files, as well as the cross-validation error graph for admixture
+# NOTE: Admixture files always output to the script location & have to be moved.
 # For gwas, remove all molinos and does LD filtering, then run `gwas_fixandtest.py`
 
 import logging
@@ -26,16 +27,20 @@ with open("../config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 genome = config["GENOME"]
-molino_sample_names=config["MOLINO_SAMPLES"]
+genome_path = config["GENOME_PATH"]
+molino_sample_names=config["MO_ONLY_SAMPLES"]
 
 original_vcf_path = config["CM_VCF_PATH"]
-original_vcf = f'{original_vcf_path}/{genome}.biallelic.filtered.vcf.gz'
+
+#This comes from the hard filter step
+#original_vcf = f'{original_vcf_path}/{genome}.all.filtered.vcf.gz'
+original_vcf = f'{original_vcf_path}/2025_Amex3.0_surface.variants.filtered.vcf.gz'
 
 #scripts go here
-#could easily make this + datapaths a function
 code_path_prefix = config["CODE_PATH"]
-code_path = f'{code_path_prefix}/popgen/scripts/nomolino'
+code_path = f'{code_path_prefix}/popgen/scripts'
 
+report_path_prefix = config["REPORT_PATH"]
 
 #outputs go here
 data_path = config["DATA_PATH"]
@@ -51,20 +56,20 @@ gwas_output_path = f'{common_output_path}/gwas'
 def generate_scripts(cmd_generator):
     sha_bang="#!/usr/bin/bash"
 
-    genome, prefix, command, n = cmd_generator
+    prefix, command, n = cmd_generator
     
     batch_info=f'\n#SBATCH --job-name={prefix} \
         \n#SBATCH --array=0-{n-1}%{n} \
         \n#SBATCH --cpus-per-task=4 \
-        \n#SBATCH --time="24-00:00" \
+        \n#SBATCH --time="2-00:00" \
         \n#SBATCH --mem=32G \
-        \n#SBATCH --mail-user=rk2643@stowers.org \
+        \n#SBATCH --mail-user=kell3262@umn.edu \
         \n#SBATCH --mail-type=FAIL \
         \n#SBATCH --mail-type=END\
-        \n#SBATCH --output=./slurmout/{prefix}.{genome}.%A.out \
-        \n#SBATCH --error=./slurmout/{prefix}.{genome}.%A.err'
+        \n#SBATCH --output=./slurmout/{prefix}.%A.out \
+        \n#SBATCH --error=./slurmout/{prefix}.%A.err'
 
-    file_name = f'{prefix}.{genome}.sh'
+    file_name = f'{prefix}.sh'
     with open(f'{code_path}/{file_name}', 'w') as file:
         file.write(f'{sha_bang}\n{batch_info}\n\n{command}')
     print(f'making {file_name} script')
@@ -75,15 +80,17 @@ def generate_scripts(cmd_generator):
 def single_script(cmd_generator):
     sha_bang="#!/usr/bin/bash"
 
-    genome, prefix, command = cmd_generator
+    prefix, command = cmd_generator
 
-    file_name = f'{prefix}.{genome}.sh'
+    file_name = f'{prefix}.sh'
     
     with open(f'{code_path}/{file_name}', 'w') as file:
         file.write(f'{sha_bang}\n\n{command}')
     print(f'making {file_name} script')
     print(f'script location: {code_path}/{file_name}')
     return(file_name)
+
+
 
 # This is some preprocessing for the vcf files that is common to both pca and gwas
 # First, rename chromosomes in original variant vcf file {genome}.biallelic.filtered.vcf.gz
@@ -97,11 +104,13 @@ def common_preplink():
     input_code = 'renamed_chr'
     #basically a key of refseqid:chr
     new_file_name= f'{original_vcf_path}/{genome}.renamechr.txt'
-    renamed_output_file = f'{data_path}/molino2/popgen/preplink/{input_code}.{genome}.vcf.gz'
+
+    renamed_output_file = f'{common_output_path}/preplink/{genome}.{input_code}.vcf.gz'
 
     #rename.txt = "old_name new_name\n"
-    accession_to_chr = f'{original_vcf_path}/chromosomes_{genome}.tsv'
-    acc_to_chr_df = pd.read_csv(accession_to_chr, usecols= ['Chromosome name','RefSeq seq accession'], sep='\t')
+    accession_to_chr = f'{genome_path}/chr_key.txt'
+    col_names = ['Chromosome name','RefSeq seq accession']
+    acc_to_chr_df = pd.read_csv(accession_to_chr, names = col_names, sep='\t')
 
 
     #need to swap the columns
@@ -132,11 +141,11 @@ def common_preplink():
         -o {renamed_output_file} {input_file}'
     #print(rename_cmd)
     
-    script_cmd = [genome, 'rename_vcf', rename_cmd, n]
+    script_cmd = ['rename_vcf', rename_cmd, n]
     generate_scripts(script_cmd)
 
 
-    #remove M2 and pair down to 25 chr
+    #pair down to 25 chr
     #input is the renamed_output_file from above
     chr_corrected_output = f'{preplink_output_path}/chr_corrected.{genome}.vcf.gz'
 
@@ -147,7 +156,7 @@ def common_preplink():
         --regions 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25'
     
     final_command =f'echo "correcting vcf"\n{correct_vcf_cmd}'
-    script_cmd= [genome, 'preplink', final_command, n]
+    script_cmd= ['preplink', final_command, n]
 
     generate_scripts(script_cmd)
     return(chr_corrected_output)
@@ -160,12 +169,11 @@ def pca_plink():
         -I {corrected_vcf_input}'
 
     #filter to 2 nocall, no M2
-    remove_m2_list = f'{preplink_output_path}/removem2.args'
     nocall2_output = f'{pca_output_path}/nocall2.{genome}.vcf.gz'
     nocall_filt_cmd = f'gatk SelectVariants\
         -V {corrected_vcf_input}\
         --max-nocall-number 2\
-        --exclude-sample-name {remove_m2_list}\
+        --exclude-sample-name M2\
         -O {nocall2_output}'
 
     window_sizes = ["50", "100", "150", "300"]
@@ -192,7 +200,7 @@ def pca_plink():
         
         #keep an eye on admixture output path...
         # aka the logs will output where you want 
-        # but the admixture files ALWAYS output to the script directory
+        # but the admixture files ##### ALWAYS output to the script directory #########
         admix_output=f'{pca_output_path}/admixture/admix.{genome}.{window}.win'
         admix_cmd=f"""for i in {{1..5}}; \n\
             do \n\
@@ -205,7 +213,7 @@ def pca_plink():
             {plink_tobed_cmd}&&\n\
             echo "running admixture"\n\
             {admix_cmd}'
-        single_script_cmd =[genome, f'{window}_fullpca_{n}', by_window_cmd]
+        single_script_cmd =[f'{window}_fullpca_{n}', by_window_cmd]
         single_script(single_script_cmd)
         n+=1
 
@@ -215,7 +223,7 @@ def pca_plink():
         {index_cmd}&&\n\
         echo "filtering to nocalls"\n\
         {nocall_filt_cmd}'
-    pre_window_script = [genome, 'pre_window_pca', pre_window_cmd]
+    pre_window_script = ['pre_window_pca', pre_window_cmd]
     pre_window_file = single_script(pre_window_script)
 
     batch_window_cmd = f'sh *_fullpca_${{SLURM_ARRAY_TASK_ID}}.{genome}.sh'
@@ -225,7 +233,7 @@ def pca_plink():
         {batch_window_cmd}'
     #print(batch_window_cmd, final_cmd)
 
-    generate_scripts([genome, f"pca_plink", final_cmd, n])
+    generate_scripts([f"pca_plink", final_cmd, n])
 
 #Plots the cross-validation error for each K in admixture run
 def plot_cv():
@@ -266,37 +274,24 @@ def plot_cv():
     #axis.set_ylim([0.4,0.55])
 
     plt.show()
-    plt.savefig(f'/n/projects/rk2643/caballo_moro_genomics/streamlined/reports/CMC_graphics_files/admixture_cross_validation_error', \
+    plt.savefig(f'{report_path_prefix}/CMC_graphics_files/admixture_cross_validation_error', \
                 dpi='figure', format=None)
     #plt.plot(k_list, cv_list)
     #plt.show()
     #plt.savefig('test', dpi='figure', format=None)
 
-"""import matplotlib.pyplot as plt
-import pandas
 
-d = {'Country': ['USA', 'USA', 'USA', 'Canada', 'Canada', 'Canada', 'Mexico', 'Mexico', 'Mexico'],
-     'Month': ['January', 'February', 'March', 'January', 'February', 'March', 'January', 'February', 'March'],
-     'GDPA': [5, 10, 5, 7, 4, 10, 9, 17, 8]}
-df = pandas.DataFrame(data=d)
-print(df)
-
-country_set = set(df['Country'])
-
-plt.figure()
-for country in country_set:
-     selected_data = df.loc[df['Country'] == country]
-     plt.plot(selected_data['Month'], selected_data['GDPA'], label=country)
-     
-plt.legend()
-plt.show()"""
                         
 # Makes plink files and preps them for GWAS tests
 def gwas_plink():
     #remove all molinos
     corrected_vcf_input = common_preplink()
 
-    ignore_molino = ','.join(molino_sample_names.values())
+    #print(type(molino_sample_names))
+    #ignore_molino = ','.join(molino_sample_names)
+    ignore_molino = molino_sample_names.replace(" ", ",")
+    print("Removing:", ignore_molino)
+
     no_molino_output = f'{gwas_output_path}/nomolino.{genome}.vcf.gz'
     remove_molino_cmd = f'bcftools view -s^{ignore_molino} \
         -o {no_molino_output} \
@@ -332,19 +327,24 @@ def gwas_plink():
         echo "making bed files"\n \
         {plink_tobed_cmd}'
     n=1
-    generate_scripts([genome, 'gwas_plink', final_cmd, n])
+    generate_scripts(['gwas_plink', final_cmd, n])
 
 
 
 if __name__=='__main__':
+
+    #pca_plink and gwas_plink will both call on common_preplink(), so only use when testing
+
     #print('generating common plink scripts')
     #common_preplink()
 
     #print('making pca scripts')
     #pca_plink()
 
-    #print('making gwas scripts')
-    #gwas_plink()
+    #plot_cv()
 
-    plot_cv()
+    print('making gwas scripts')
+    gwas_plink()
+
+    
 
